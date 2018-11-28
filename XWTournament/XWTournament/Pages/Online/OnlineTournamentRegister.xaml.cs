26668f828ef46ccc88b287342499cecf4640999e
aaron.sayles <aaron.sayles@gmail.com>
2018-11-28 15:21:48 -0600

    20181128

--- XWTournament/XWTournament/Pages/Online/OnlineTournamentRegister.xaml.cs
+++ XWTournament/XWTournament/Pages/Online/OnlineTournamentRegister.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RestSharp;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,15 +7,46 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using XWTournament.Classes;
 
 namespace XWTournament.Pages.Online
 {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class OnlineTournamentRegister : ContentPage
 	{
-		public OnlineTournamentRegister ()
+        RestClient client = Utilities.InitializeRestClient();
+
+        public OnlineTournamentRegister ()
 		{
 			InitializeComponent ();
+
+            if (App.IsUserLoggedIn)
+            {
+                searchButton.IsVisible = true;
+            }
+            else
+            {
+                searchButton.IsVisible = false;
+            }
 		}
-	}
+
+        private async void searchButton_ClickedAsync(object sender, EventArgs e)
+        {
+            this.IsBusy = true;
+            loadingOverlay.IsVisible = true;
+
+            //Search tournaments open to the public
+            RestRequest request = new RestRequest("Players/{userid}", Method.GET);
+            request.AddUrlSegment("userid", App.CurrentUser.Id);
+
+            // execute the request
+            var response = await client.ExecuteTaskAsync(request);
+            string content = response.Content;
+
+            //List<Player> lstApiPlayers = JsonConvert.DeserializeObject<List<Player>>(JsonConvert.DeserializeObject(content).ToString());
+
+            this.IsBusy = false;
+            loadingOverlay.IsVisible = false;
+        }
+    }
 }
